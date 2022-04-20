0fd7008278e8b216faf3b03929d4693d074e2ddf
typhelps2 <60396757+typhelps2@users.noreply.github.com>
2022-04-19 21:42:17 -0400

    Add files via upload

---
+++ scs141-program-4-typhelps2-main/Program4/SecondviewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondviewController.swift
+//  Program4
+//
+//  Created by Tyler Phelps on 4/15/21.
+//
+
+import UIKit
+
+class SecondviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ChangeBackground(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ scs141-program-4-typhelps2-main/Program4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Program4
+//
+//  Created by Tyler Phelps on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
